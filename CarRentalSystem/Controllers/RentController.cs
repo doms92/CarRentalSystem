3739d97b31f154e67e2e46eb8fa6510932c6d1a3
doms92 <domos92@gmail.com>
2021-08-28 12:30:20 -0700

    Added Rent Controller

---
+++ CarRentalSystem/Controllers/RentController.cs
@@ -0,0 +1,29 @@
+﻿using CarRentalSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRentalSystem.Controllers
+{
+    public class RentController : Controller
+    {
+
+        supercar db = new supercar();
+        //
+        // GET: Rent
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Getcar()
+        {
+            var car = db.carregs.ToList();
+
+            return Json(car, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
